38546ae4478d4b6df1abd5029dfe8419147461e6
francis556 <vikrammehra244@gmail.com>
2022-05-01 19:05:09 +0200

    objects

---
+++ constructs/index.php
@@ -0,0 +1,97 @@
+<?php
+
+
+class Team
+
+{
+    protected $name; 
+
+    protected $members = []; 
+
+    public function __construct($name, $members = [])
+
+    {
+        $this->name = $name;
+
+        $this->members = $members;
+
+    }
+
+    public static function start(...$params)
+
+    {
+        var_dump($params);
+
+        return new static(...$params);
+    }
+
+    public function name()
+
+    {
+        return $this->name;
+
+    }
+
+    public function members()
+
+    {
+        return $this->members;
+
+    }
+
+    public function add($name)
+
+    {
+        $this->members[] = $name;
+
+    }
+
+    public function cancle()
+
+    {
+
+    }
+
+    public function manager()
+
+    {
+
+    }
+
+    
+
+}
+
+
+class  Member
+
+{
+
+    protected $name;
+
+    public function __construct($name)
+
+    {
+
+        $this->name = $name;
+    }
+
+    public function lastViewed()
+
+    {
+
+
+    }
+        
+}
+
+$acme = Team::start('Acme', [
+
+    new Member('John Doe'),
+
+    new Member('Jane Doe')
+
+]);
+
+
+var_dump($acme->members());
