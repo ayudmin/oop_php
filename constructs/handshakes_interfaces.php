a66383f21bbba99a0a76e0f3521304735bb735f0
francis556 <vikrammehra244@gmail.com>
2022-05-07 22:00:12 +0200

    handshakes and interfaces

---
+++ constructs/handshakes_interfaces.php
@@ -0,0 +1,47 @@
+<?php
+
+
+interface Newsletter 
+
+{
+    public function subscribe($email);
+}
+
+
+class CampaignMonitor implements Newsletter
+
+{
+
+    public function subscribe($email)
+
+    {
+        die('subscribing with campaign monitor');
+    }
+}
+
+class Drip implements Newsletter
+
+{
+    public function subscribe($email)
+
+    {
+        die('subscribing with drip');
+    }   
+}
+
+
+class NewsletterSubscriptionsController
+
+{
+    public function store(Newsletter $newsletter)
+
+    {
+        $email = 'joe@example.com';
+
+        $newsletter->subscribe($email);
+    }
+}
+
+$controller = new NewsletterSubscriptionsController();
+
+$controller->store(new Drip());
