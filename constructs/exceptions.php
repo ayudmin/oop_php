b3c308558116bea144e33ccd17f5ec708f60ad98
francis556 <vikrammehra244@gmail.com>
2022-05-29 16:10:40 +0200

    exceptions

--- constructs/exceptions.php
+++ constructs/exceptions.php
@@ -2,44 +2,76 @@
 
 
 
-class Video
+class MaximunMembersReached extends Exception
+
 {
+    protected $message = 'You may not add more than 3 members';
+
+
+}
+class Member 
+
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 
 
 }
 
-class User
+class Team
 
 {
 
-    public function download(Video $video)
+    protected $members = [];
+
+    public function add(Member $member)
+
     {
-        if (! $this->subscribed()){
-            throw new Exception('You must be subscribed.');
+        if (count($this->members) === 3){
+
+            throw new MaximunMembersReached;
         }
 
-
+        $this->members[] = $member;
     }
 
-    public function subscribed()
-    {
-        return false;  
 
+    public function members()
+
+    {
+        return $this->members;
     }
 }
 
 
-class UserDownloadController
+class TeamMembersController
 
 {
-    public function show()
+
+    public function store()
     {
-        try {
+        $team = new Team;
 
-            (new User)->download(new Video);
-        } catch {
+    try {
 
-            echo 'Video download not allowed';
-        }
+        $team->add(new Member('Joe Doe'));
+        $team->add(new Member('Pita Doe'));
+        $team->add(new Member('Frank Doe'));
+        $team->add(new Member('Lee Doe'));
+
+        var_dump($team->members());
+    } catch (MaximunMembersReached $e) {
+
+        var_dump($e->getMessage());
+    }
+
     }
 }
+
+(new TeamMembersController)->store();
+
+
